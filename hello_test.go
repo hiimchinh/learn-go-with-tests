d97ee86316c837b8d50ebb47db26210b32ebc211
hiimchinh <chinhhm@proton.me>
2025-12-14 00:01:56 +0700

    refactor test cases

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chinh")
-	want := "Hello, Chinh"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chinh")
+		want := "Hello, Chinh"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("say 'Hello, World' when an empty string is suplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
